bfc553789a604d6659e28c91bbb061ea238dd2bf
Nikolay Panichev <n.panichev@beltel.ru>
2021-08-24 17:26:38 +0300

    Add files

---
+++ MySuperWebSite/MyWebPage1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(reguest):
+    return HttpResponse("<center><h2>Hello World from Python DJANGO!!!</h2></center>")
